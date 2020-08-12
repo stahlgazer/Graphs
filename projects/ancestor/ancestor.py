edc3bd206a2c7fd9148366e01fc9b2c90b4b36d3
Gavin S <stahlgavin@gmail.com>
2020-08-12 13:58:51 -0700

    day2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,28 @@
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    
+    q = Queue()
+    current_node = starting_node
+    connections = {}
+    
+    for item in ancestors: 
+        parent = item[0]
+        child = item[1]
+
+        if child not in connections:
+            connections[child] = set()
+        connections[child].add(parent)
+
+    if starting_node in connections:
+        q.enqueue(connections[current_node])
+    else:
+        return -1
+
+    while True:
+        family = q.dequeue()
+        current_node = min(family)
+        if current_node not in connections:
+            return current_node
+        else:
+            q.enqueue(connections[current_node])
